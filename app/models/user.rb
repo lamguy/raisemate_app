078277d5f1d18a06758363fe8898a08940cf73ef
Lam Nguyen <me@madebylam.com>
2011-08-13 23:03:28 -0500

    Improved user form for validation password and all required fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+  validates :user_login, :presence => true
+  validates :user_name, :presence =>true
+  validates :user_email, :presence => true
+  validates :user_password, :presence => true,
+                            :confirmation => true,
+                            :length => {:within => 6..40},
+                            :on => :create
+  validates :user_password, :presence => true,
+                            :confirmation => true,
+                            :length => {:within => 6..40},
+                            :allow_blank => true,
+                            :on => :update
+                        
 end
